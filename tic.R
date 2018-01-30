78c212ac7db18dfe56fc33bc61590c7344850d02
Kirill Müller <krlmlr+r@mailbox.org>
2018-01-30 02:39:37 +0100

    debug = TRUE for safety

--- tic.R
+++ tic.R
@@ -26,8 +26,11 @@ if (Sys.getenv("FIGSHARE_API") != "") {
           title = unname(desc::desc_get("Title")),
           description = unname(desc::desc_get("Description")),
           type = "fileset",
-          session = api_key
+          session = api_key,
+          debug = TRUE
         )
+        httr::stop_for_status(id, "create article")
+        id <- httr::content(id)
         message("Created article ", id)
 
         # Workaround for ropenscilabs/tic#38
